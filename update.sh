9e676f8b8af9c958c98be0eed1dd067e68a42224
zs <shuizhang654@gmail.com>
2025-10-09 08:52:31 +0000

    update

--- update.sh
+++ update.sh
@@ -8,4 +8,6 @@ jq -r '.nodes.[]|select(.ping!="0")| .link' output.json >> ./tmp
 done < ./list.txt
 
 
+./lite --test ./tmp --config ./config.json
+jq -r '.nodes.[]|select(.ping!="0")| .link' output.json > ./tmp
 sort -u ./tmp > nodes
